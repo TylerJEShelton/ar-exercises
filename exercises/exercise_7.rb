6ec01854ef97daa37d13102014e3efac1812b88a
Tyler Shelton <tylershelton99@gmail.com>
2022-01-07 12:13:50 -0500

    Complete exercise 7 in the active record assignment. w9m23 - More AR - Stores and Employees - LHL

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,22 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+@new_employee = @store2.employees.create(first_name: "Monique", last_name: "DeCharme", hourly_rate: 200)
+puts(@new_employee.valid?)
+
+@test_employee = Employee.create(first_name: "This is", last_name: "A Test", hourly_rate: 80)
+puts(@test_employee.errors[:first_name])
+puts(@test_employee.errors[:last_name])
+puts(@test_employee.errors[:hourly_rate])
+puts(@test_employee.errors[:store_id])
+
+print("Enter a new store name: > ")
+user_input = gets.chomp
+puts (user_input)
+
+@new_store = Store.create(name: user_input, annual_revenue: 400000)
+puts(@new_store.errors[:name])
+puts(@new_store.errors[:annual_revenue])
+puts(@new_store.errors[:mens_apparel])
+puts(@new_store.errors[:womens_apparel])
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
     has_many :employees
+
+    validates :name, length: { minimum: 3 }
+    validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+    validate :must_sell_mens_or_womens_clothes
+
+    def must_sell_mens_or_womens_clothes
+        if !mens_apparel && !womens_apparel
+            errors.add(:mens_apparel, "must sell mens or womens apparel")
+            errors.add(:womens_apparel, "must sell mens or womens apparel")
+        end
+    end
 end
