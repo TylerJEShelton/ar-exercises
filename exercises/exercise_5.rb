f8aa1cd0a5867c7e1cddc3420618e923c1bf8c69
Tyler Shelton <tylershelton99@gmail.com>
2022-01-07 11:11:31 -0500

    Complete exercise 5 in the active record assignment. w9m23 - More AR - Stores and Employees - LHL

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts("The company's total revenue is $#{Store.sum(:annual_revenue)}")
+puts("The average revenue per store is $#{Store.average(:annual_revenue)}")
+puts("There are #{Store.where(["annual_revenue >= 1000000"]).count} stores that generate over $1,000,000 in revenue.")
